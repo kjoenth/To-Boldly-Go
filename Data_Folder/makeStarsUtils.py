b86bf839fc85d6971aef0b6a21feee9ac67154dc
seanth <seanth@gmail.com>
2018-10-04 23:12:30 -0500

    Star generation working
    Everything related to star generation—except for mass—>color calls—has
    been converted. Whether it is all working correctly remains to be
    tested.

--- Data_Folder/makeStarsUtils.py
+++ Data_Folder/makeStarsUtils.py
@@ -23,14 +23,22 @@ theGasGiantTemplate = ""
 theRingsTemplate = ""
 theOceanTemplate = ""
 thePlanetTemplate = ""
-theStarTmp = ""
-
+theStarTemplate = ""
+theGalaxyTemplate = ""
+theGalaxyText = ""
 theWikiTemplate = ""
+theWikiText = ""
 theResearchBodyTemplate = ""
+GTYPE = 1
+CLUSTERNUM = 0
+
 #############################
 
-
 def makeAStar(star_MassKG, star_Name, star_Description):
+    global theWikiText
+    global GTYPE
+    global CLUSTERNUM
+    global theGalaxyText
     #consder moving this to an imported file. STH 2017-0403
     star_MassKSP = astroUtils.sol2Kerbol_kg(star_MassKG)
     star_MassSolar = astroUtils.kg2solarMass(star_MassKG)
@@ -77,14 +85,20 @@ def makeAStar(star_MassKG, star_Name, star_Description):
 #     theG = rGREENb / 255.0
 #     theB = rgBLUE / 255.0
 #     #===convert to hex and prefix with zero if needed'
-#     hexR$ = HEX$(REDgb)
-#     hexG$ = HEX$(rGREENb)
-#     hexB$ = HEX$(rgBLUE)
-#     IF LEN(hexR$) = 1 THEN hexR$ = "0" + hexR$
-#     IF LEN(hexG$) = 1 THEN hexG$ = "0" + hexG$
-#     IF LEN(hexB$) = 1 THEN hexB$ = "0" + hexB$
+#     hexR = HEX(REDgb)
+#     hexG = HEX(rGREENb)
+#     hexB = HEX(rgBLUE)
+#     if LEN(hexR) = 1: hexR = "0" + hexR
+#     if LEN(hexG) = 1: hexG = "0" + hexG
+#     if LEN(hexB) = 1: hexB = "0" + hexB
+    theR = 1.0
+    theG = 1.0
+    theB = 1.0
+    hexR = "needs "
+    hexG = "to be "
+    hexB = "converted"
 #     #======='
-#     star_HTMLColour$ = hexR$ + hexG$ + hexB$
+    star_HTMLColour = hexR + hexG + hexB
     star_Circumference = astroUtils.starCircumference(star_RadiusKSP) #use KSP size
     star_SurfaceArea = astroUtils.starSurfaceArea(star_RadiusKSP) #use KSP size
     star_Volume = astroUtils.starVolume(star_RadiusKSP) #use KSP size
@@ -104,137 +118,157 @@ def makeAStar(star_MassKG, star_Name, star_Description):
     ########################'
     ###Fill in property data'
     aPropertiesNode = thePropertiesTemplate
-    aPropertiesNode =  aPropertiesNode % {'theDescription':star_Description, 'theRadius':star_RadiusKSP, 'theMass':star_MassKSP, 'theGravParameter':star_stdGravitationalParameter, 'theGeeASL':star_surfaceGravity, 'doesRotate':"True", 'rotationPeriod':star_RotationalPeriod, 'initialRotation':0.0, 'isTidallyLocked':"False", 'isHomeWorld':"False", 'theSphereOfInfluence':star_SOI}
+    aPropertiesNode =  aPropertiesNode % {'theDescription':star_Description, 'theRadius':star_RadiusKSP, 'theMass':star_MassKSP, 
+        'theGravParameter':star_stdGravitationalParameter, 'theGeeASL':star_surfaceGravity, 'doesRotate':"True", 
+        'rotationPeriod':star_RotationalPeriod, 'initialRotation':0.0, 'isTidallyLocked':"False", 'isHomeWorld':"False", 
+        'theSphereOfInfluence':star_SOI}
     activeProperties = ["description", "radius", "mass", "gravParameter", "geeASL", "rotates", "rotationPeriod", "sphereOfInfluence"]
     for aProp in activeProperties:
         aPropertiesNode = aPropertiesNode.replace("//%"+aProp, "%"+aProp)
-    print aPropertiesNode
     ###End property data'
     ########################'
 
-
-#     ########################'
-#     ###Fill in orbit data'
-#     IF (GTYPE = 0 OR GTYPE = 1) THEN
-#         theReferenceBody$ = "Core"
-#         IF GTYPE = 0 THEN theInclination$ = STR$(INT(RND * 360))
-#         IF GTYPE = 1 THEN theInclination$ = STR$(INT(RND * 25) + 1)
-#         theSemiMajorAxis$ = STR$(star_semimajorAxis)
-#     END IF
-#     IF (GTYPE = 2) THEN
-#         theReferenceBody$ = STR$(INT(RND * CLUSTERNUM))
-#         theInclination$ = STR$(INT(RND * 360))
-#         theSemiMajorAxis$ = STR$(star_semimajorAxis)
-#     END IF
-#     theArgumentOfPeriapsis = INT(RND * 360)
-#     theMeanAnomalyAtEpoch$ = STR$(0)
-#     theEpoch$ = STR$(0)
-#     theMode$ = "0" 'STR$(0)
-#     theColour$ = STR$(theR) + "," + STR$(theG) + "," + STR$(theB) + ",1.0"
-#     aOrbitTemp$ = theOrbitTemplate$
-#     #FUNCTION orbitNode$ (aTemplate$, theReferenceBody$, theColour$, theMode$, theInclination$, theEccentricity$, theSemiMajorAxis$, theLongitudeOfAscendingNode$, STR$(theArgumentOfPeriapsis), theMeanAnomalyAtEpoch$, theEpoch$)
-#     aOrbitNode$ = orbitNode$(aOrbitTemp$, theReferenceBody$, theColour$, theMode$, theInclination$, theEccentricity$, theSemiMajorAxis$, theLongitudeOfAscendingNode$, str$(theArgumentOfPeriapsis), theMeanAnomalyAtEpoch$, theEpoch$)
-#     ###End orbit data'
-#     ########################'
-
-
-#     ########################'
-#     ###Fill in light data'
-#     sunlightColor$ = STR$(theR) + "," + STR$(theG) + "," + STR$(theB) + ",1.0"
-#     sunlightIntensity$ = "0.50"
-#     scaledSunlightColor$ = STR$(theR) + "," + STR$(theG) + "," + STR$(theB) + ",1.0"
-#     scaledSunlightIntensity$ = "0.30"
-#     IVASuncolor$ = STR$(theR) + "," + STR$(theG) + "," + STR$(theB) + ",1.0"
-#     IVASunIntensity$ = "1.0"
-#     'sunLensFlareColor$ = "0.3,0,0,1.0"
-#     #adjust some values to cover over the default yellow'
-#     IF theB >= 0.9 THEN
-#         theRFlare = theR / 2
-#         sunLensFlareColor$ = STR$(theRFlare) + "," + STR$(theG) + "," + STR$(theB) + ",1.0"
-#     END IF
-#     IF NOT (theB >= 0.9) THEN
-#         sunLensFlareColor$ = STR$(theR) + "," + STR$(theG) + "," + STR$(theB) + ",1.0"
-#     END IF
-#     ambientLightColor$ = "0,0,0,1"
-#     'New star radius/Kerbol radius
-#     sunAUMultiplier = (star_RadiusKSP/261600000.0)
-#     # 13599840256m = Kerbin SMA
-#     sunAU$ = str$(sunAUMultiplier*13599840256)
-#     luminosity$ = str$(star_Lum*1367) #just multiply the star's solar luminosity by solar constant for earth (1367)
-#     givesOffLight$ = "True"
-#     #############################################################################
-#     #new routine to alter the apparent magnitude of stars in the Kerbin night sky
-#     #skyBrightness = e^(-3.897121 - 0.1868262*apparentMagnitude) where e is 2.718282
-#     #0.005 is ~ the lowest value to be seen in Kerbin's night sky
-#     #3.0 is ~ the size of Kerbol in the sky
-#     skyBrightness = 2.718282^(-3.897121 - 0.1868262*star_ApparentMag)
-#     if skyBrightness <0.005 then
-#         skyBrightness = 0.0045
-#     end if 
-#     #############################################################################
-#     aLightTemp$ = theLightTemplate$
-#     aLightNode$ = lightNode$(aLightTemp$, sunlightColor$, sunlightIntensity$, scaledSunlightColor$, scaledSunlightIntensity$, IVASuncolor$, IVASunIntensity$, sunLensFlareColor$, ambientLightColor$, sunAU$, luminosity$, givesOffLight$, str$(skyBrightness))
-#     ###End light data'
-#     ########################'
+    ########################'
+    ###Fill in orbit data'
+    if (GTYPE == 0 or GTYPE == 1):
+        theReferenceBody = "Core"
+        if GTYPE == 0: 
+            theInclination = random.randint(0,360)
+        if GTYPE == 1: 
+            theInclination = random.randint(0,25)
+        theSemiMajorAxis = star_semimajorAxis
+    
+    if (GTYPE == 2):
+        theReferenceBody = random.randint(0,CLUSTERNUM)
+        theInclination = random.randint(0,360)
+        theSemiMajorAxis = star_semimajorAxis
+    
+    theEccentricity = ""
+    theLongitudeOfAscendingNode = ""
+    theArgumentOfPeriapsis = random.randint(0,360)
+    theMeanAnomalyAtEpoch = 0
+    theEpoch = 0
+    theMode = 0 
+    theColour = "%f, %f, %f, 1.0" % (theR, theG, theB)
+    aOrbitNode = theOrbitTemplate
+    aOrbitNode = aOrbitNode % {'theReferenceBody':theReferenceBody, 'theColour':theColour, 'theMode':theMode, 'theInclination':theInclination, 
+        'theEccentricity':theEccentricity, 'theSemiMajorAxis':theSemiMajorAxis, 'theLongitudeOfAscendingNode':theLongitudeOfAscendingNode, 
+        'theArgumentOfPeriapsis':theArgumentOfPeriapsis, 'theMeanAnomalyAtEpoch':theMeanAnomalyAtEpoch, 'theEpoch':theEpoch} 
+    activeProperties = ["referenceBody", "color", "mode", "inclination", "semiMajorAxis", "argumentOfPeriapsis", "meanAnomalyAtEpoch", "epoch"]
+    for aProp in activeProperties:
+        aOrbitNode = aOrbitNode.replace("//%"+aProp, "%"+aProp)
+    ###End orbit data'
+    ########################'
 
 
-#     ########################'
-#     ###Fill in material data'
-#     'emitColorZero$ = "0.6,0.3,0.0,1.0"
-#     emitColorZero$ = STR$(theR) + "," + STR$(theG) + "," + STR$(theB) + ",1.0"
-#     'emitColorOne$ = "0.9,0.1,0.0,1.0"
-#     emitColorOne$ = STR$(theR * 0.4) + "," + STR$(theG * 0.4) + "," + STR$(theB * 0.4) + ",1.0"
-#     'sunspotColor$ = "1.0,0,0,1.0"
-#     sunspotColor$ = "0.23,0.23,0.23,1.0"
-#     'rimColor$ = "0.68,0.05,0.05,1.0"
-#     rimColor$ = STR$(theR) + "," + STR$(theG) + "," + STR$(theB) + ",1.0"
-#     rimPower$ = "1"
-#     rimBlend$ = "1"
-#     aMaterialTemp$ = theMaterialTemplate$
-#     aMaterialNode$ = materialNode$(aMaterialTemp$, emitColorZero$, emitColorOne$, sunspotColor$, rimColor$, rimPower$, rimBlend$)
-#     ###End material data'
-#     ########################'
+    ########################'
+    ###Fill in light data'
+    sunlightColor = "%f, %f, %f, 1.0" % (theR, theG, theB)
+    sunlightIntensity = "0.50"
+    scaledSunlightColor = "%f, %f, %f, 1.0" % (theR, theG, theB)
+    scaledSunlightIntensity = "0.30"
+    IVASuncolor = "%f, %f, %f, 1.0" % (theR, theG, theB)
+    IVASunIntensity = "1.0"
+    #sunLensFlareColor = "0.3,0,0,1.0"
+    #adjust some values to cover over the default yellow'
+    if theB >= 0.9:
+        theRFlare = theR / 2
+        sunLensFlareColor = "%f, %f, %f, 1.0" % (theRFlare, theG, theB)
+    
+    if not (theB >= 0.9):
+        sunLensFlareColor = "%f, %f, %f, 1.0" % (theR, theG, theB)
+    
+    ambientLightColor = "0,0,0,1"
+    #New star radius/Kerbol radius
+    sunAUMultiplier = star_RadiusKSP/261600000.0
+    # 13599840256m = Kerbin SMA
+    sunAU = sunAUMultiplier*13599840256
+    luminosity = star_Lum*1367 #just multiply the star's solar luminosity by solar constant for earth (1367)
+    givesOffLight = "True"
+    #############################################################################
+    #new routine to alter the apparent magnitude of stars in the Kerbin night sky
+    #skyBrightness = e^(-3.897121 - 0.1868262*apparentMagnitude) where e is 2.718282
+    #0.005 is ~ the lowest value to be seen in Kerbin's night sky
+    #3.0 is ~ the size of Kerbol in the sky
+    skyBrightness = 2.718282**(-3.897121 - 0.1868262*star_ApparentMag)
+    if skyBrightness <0.005:
+        skyBrightness = 0.0045
+
+    #############################################################################
+    aLightNode = theLightTemplate
+    aLightNode = aLightNode % {'theSunlightColor':sunlightColor, 'theSunlightIntensity':sunlightIntensity, 'theScaledSunlightColor':scaledSunlightColor, 
+        'theScaledSunlightIntensity':scaledSunlightIntensity, 'theIVASuncolor':IVASuncolor, 'theIVASunIntensity':IVASunIntensity, 
+        'theSunLensFlareColor':sunLensFlareColor, 'theAmbientLightColor':ambientLightColor, 'theSunAU':sunAU, 'theLuminosity':luminosity, 
+        'theGivesOffLight':givesOffLight, 'skyBrightness':skyBrightness} 
+
+    activeProperties = ["sunlightColor", "sunlightIntensity", "scaledSunlightColor", "scaledSunlightIntensity", "IVASuncolor", "IVASunIntensity", 
+        "sunLensFlareColor", "ambientLightColor", "sunAU", "luminosity", "givesOffLight", "key"]
+    for aProp in activeProperties:
+        aLightNode = aLightNode.replace("//%"+aProp, "%"+aProp)
+    aLightNode = aLightNode.replace("//key", "key")
+    ###End light data'
+    ########################'
 
 
-#     ########################'
-#     ###Fill in coronas data'
-#     #https://en.wikipedia.org/wiki/Stellar_classification
-#     # ELSE statements don't seem to work in included code
-#     IF (star_MassSolar >= 16) THEN
-#         starColour$ = "Blue"
-#     END IF
-#     IF (star_MassSolar < 16 AND star_MassSolar >= 2.1) THEN
-#         starColour$ = "Blue"
-#     END IF
-#     IF (star_MassSolar < 2.1 AND star_MassSolar >= 1.4) THEN
-#         starColour$ = "White"
-#     END IF
-#     IF (star_MassSolar < 1.4 AND star_MassSolar >= 1.04) THEN
-#         starColour$ = "Yellow"
-#     END IF
-#     IF (star_MassSolar < 1.04 AND star_MassSolar >= 0.8) THEN
-#         starColour$ = "Orange"
-#     END IF
-#     IF (star_MassSolar < 0.8 AND star_MassSolar >= 0.45) THEN
-#         starColour$ = "Red"
-#     END IF
-#     IF (star_MassSolar < 0.45 AND star_MassSolar >= 0.08) THEN
-#         starColour$ = "Brown"
-#     END IF
-#     IF (star_MassSolar < 0.08) THEN
-#         starColour$ = "BlackHole"
-#     END IF
-
-#     aCoronaTemp$ = theCoronasTemplate$
-#     aCoronaNode$ = coronaNode$(aCoronaTemp$, starColour$)
-#     ###End coronas data'
-#     ########################'
+    ########################'
+    ###Fill in material data'
+    #emitColorZero = "0.6,0.3,0.0,1.0"
+    #emitColorOne = "0.9,0.1,0.0,1.0"
+    #sunspotColor = "1.0,0,0,1.0"
+    #rimColor = "0.68,0.05,0.05,1.0"
+    emitColorZero = "%f, %f, %f, 1.0" % (theR, theG, theB)
+    emitColorOne = "%f, %f, %f, 1.0" % (theR*0.4, theG*0.4, theB*0.4)
+    sunspotColor = "0.23,0.23,0.23,1.0"
+    rimColor = "%f, %f, %f, 1.0" % (theR, theG, theB)
+    rimPower = "1"
+    rimBlend = "1"
+    aMaterialNode = theMaterialTemplate
+    aMaterialNode = aMaterialNode % {'theEmitColor0':emitColorZero, 'theEmitColor1':emitColorOne, 'theSunspotColor':sunspotColor,
+        'theRimColor':rimColor, 'theRimPower':rimPower, 'theRimBlend':rimBlend}
+    ###End material data'
+    ########################'
 
 
-    # aStarTemp = theStarTmp
-    # #'FUNCTION starTempSubstitution$ (aTemplate$, aName$, aPropertiesNode$, aOrbitNode$, aRingNode$, aLightNode$, aMaterialNode$, aCoronaNode$, aSolarPowerCurve$)
-    # aStarTmp = aStarTemp % (star_Name, aPropertiesNode, aOrbitNode, "", aLightNode, aMaterialNode, aCoronaNode, "")
-    # print aStarTmp
+    ########################'
+    ###Fill in coronas data'
+    #https://en.wikipedia.org/wiki/Stellar_classification
+    if (star_MassSolar >= 16):
+        starColour = "Blue"
+    
+    if (star_MassSolar < 16 and star_MassSolar >= 2.1):
+        starColour = "Blue"
+    
+    if (star_MassSolar < 2.1 and star_MassSolar >= 1.4):
+        starColour = "White"
+    
+    if (star_MassSolar < 1.4 and star_MassSolar >= 1.04):
+        starColour = "Yellow"
+    
+    if (star_MassSolar < 1.04 and star_MassSolar >= 0.8):
+        starColour = "Orange"
+    
+    if (star_MassSolar < 0.8 and star_MassSolar >= 0.45):
+        starColour = "Red"
+    
+    if (star_MassSolar < 0.45 and star_MassSolar >= 0.08):
+        starColour = "Brown"
+    
+    if (star_MassSolar < 0.08):
+        starColour = "BlackHole"
+    
+    aCoronaNode = theCoronasTemplate
+    aCoronaNode = aCoronaNode % {'theTexture':starColour}
+    ###End coronas data'
+    ########################'
+
+    ########################'
+    ###Use generated nodes to fill in star template
+    aStarTmp = theStarTemplate
+    aStarTmp = aStarTmp % {'theName':star_Name, 'theProperties':aPropertiesNode, 'theOrbitData':aOrbitNode, 'theRingData':'', 'theLightData':aLightNode, 'theMaterialData':aMaterialNode, 'theCoronasData':aCoronaNode, 'theSolarPowerCurve':''}
+    activeProperties = ['name', 'Properties', 'Orbit', 'Light', 'Material', 'Coronas']
+    for aProp in activeProperties:
+        aStarTmp = aStarTmp.replace("//%"+aProp, "%"+aProp)
+    theGalaxyText = theGalaxyText + "\n" + aStarTmp
 
 
 #     ########################
@@ -243,28 +277,34 @@ def makeAStar(star_MassKG, star_Name, star_Description):
 #     mediumCutoff = 5.5
 #     hardCutoff = 4.5
 #     if (star_ApparentMag >=normalCutoff) then
-#         theIgnoreLevel$ = "true false false false"
+#         theIgnoreLevel = "true false false false"
 #     end if
-#     if (star_ApparentMag <normalCutoff AND star_ApparentMag >=mediumCutoff) then
-#         theIgnoreLevel$ = "true true false false"
+#     if (star_ApparentMag <normalCutoff and star_ApparentMag >=mediumCutoff) then
+#         theIgnoreLevel = "true true false false"
 #     end if
-#     if (star_ApparentMag <mediumCutoff AND star_ApparentMag >=hardCutoff) then
-#         theIgnoreLevel$ = "true true true false"
+#     if (star_ApparentMag <mediumCutoff and star_ApparentMag >=hardCutoff) then
+#         theIgnoreLevel = "true true true false"
 #     end if
 #     if (star_ApparentMag <hardCutoff) then
-#         theIgnoreLevel$ = "true true true true"
+#         theIgnoreLevel = "true true true true"
 #     end if
 
 #     #Maybe make the discovery text more like the wiki entries?
 #     #STH 2018-0704
-#     localizationText$ = localizationText$+ "        #autoLOC_RBodies_discovery_"+star_Name$+" = "+star_Name$ + ": Approximately" + str$(star_distToKerbol/1e9) +" Gm from Kerbol. Apparent magnitude of"+str$(star_ApparentMag)+". "+ star_Description$ +chr$(10)
-#     discoveryText$ = discoveryText$+"        #autoLOC_RBodies_discovery_"+star_Name$+chr$(10)
-#     ignoreLevels$ = ignoreLevels$ + "        "+star_Name$ +" = "+theIgnoreLevel$+chr$(10)
+#     localizationText = localizationText+ "        #autoLOC_RBodies_discovery_"+star_Name+" = "+star_Name + ": Approximately" + str(star_distToKerbol/1e9) +" Gm from Kerbol. Apparent magnitude of"+str(star_ApparentMag)+". "+ star_Description +chr(10)
+#     discoveryText = discoveryText+"        #autoLOC_RBodies_discovery_"+star_Name+chr(10)
+#     ignoreLevels = ignoreLevels + "        "+star_Name +" = "+theIgnoreLevel+chr(10)
 
 
-#     ########################
-#     ###Fill in the wiki data
-#     aWikiTemplate$ = theWikiTemplate$
-#     aWikiTemplate$ = wikiEntry$(aWikiTemplate$, star_HTMLColour$, star_Name$, star_RadiusKSP, star_Circumference, star_SurfaceArea, star_MassKSP, star_stdGravitationalParameter, star_Density, star_surfaceGravity, star_escapeVelocity, star_RotationalPeriod, star_siderealRotationalVel, star_theSynchronousOrbit, star_SOI, star_FrostLineKSP*1000, star_TempK, star_Lum, star_AbsMag, star_semimajorAxis, star_distToKerbol, star_ApparentMag)
-#     PRINT #10, aWikiTemplate$
-# END SUB
+    ########################
+    ###Fill in the wiki data
+    aWikiTemplate = theWikiTemplate
+    aWikiTemplate = aWikiTemplate % {'htmlColour':star_HTMLColour , 'starName':star_Name, 'theRadius':star_RadiusKSP, 
+        'theCircumfrence':star_Circumference, 'theSurfaceArea':star_SurfaceArea, 'theMass':star_MassKSP, 
+        'theStdGrav':star_stdGravitationalParameter, 'theDensity':star_Density, 'theSurfaceGravity':star_surfaceGravity, 
+        'theEscapeVelocity':star_escapeVelocity, 'theRotationPeriod':star_RotationalPeriod, 'theSideralVelocity':star_siderealRotationalVel, 
+        'theSynchronousOrbit':star_theSynchronousOrbit, 'theSOI':star_SOI, 'theFrostLine':star_FrostLineKSP, 'theTemp':star_TempK, 
+        'theLuminosity':star_Lum, 'theAbsMagnitude':star_AbsMag, 'theApparentMagnitude':star_ApparentMag, 'theSMA':star_semimajorAxis, 
+        'theDistToKerbol':star_distToKerbol}
+    theWikiText = theWikiText + "\n" + aWikiTemplate
+    #print theWikiText
